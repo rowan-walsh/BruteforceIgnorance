1924a30c07c44426b361dd4ca00dbc1cfbc7f542
Scott Lawson <scottlawsonbc@gmail.com>
2013-07-10 20:02:25 -0700

    Added simulation code

---
+++ Thermodynamics Code/Material.fs
@@ -0,0 +1,14 @@
+﻿namespace Simulator
+
+module Material = 
+
+    open System
+    open Simulator.UnitsOfMeasure
+    open Simulator.PhysicalConstants
+
+    /// A material with thermodynamic properties
+    type Material(specificHeat, conductance, density, emissivity) =
+        member this.SpecificHeat : float<C> = specificHeat
+        member this.Conductance : float<k> = conductance
+        member this.Density : float<kg/m^3> = density
+        member this.Emissivity : float = emissivity
---
+++ Thermodynamics Code/SimulationResult.fs
@@ -0,0 +1,13 @@
+﻿namespace Simulator
+
+[<AutoOpenAttribute()>]
+module SimulationResult =
+    open UnitsOfMeasure
+
+    type Result = 
+        {
+        data: float[]
+        duration: float<s>
+        equilibriumReached: bool
+        }
+
